e42ef2c372ba513635ad5494d95094551e2d2592
Syed Asad Raza <111901981+AsadAnalyst@users.noreply.github.com>
2025-09-20 15:34:20 +0500

    Fix typo in Tab.Screen closing tag

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ function MainTabs() {
       <Tab.Screen name="Charts" component={ChartsScreen} />
       <Tab.Screen name="Settings">
         {() => <SettingsScreenWithSignOut />}
-      </Tab.Screen>
+      </Tab.Screenn>
     </Tab.Navigator>
   );
 }
